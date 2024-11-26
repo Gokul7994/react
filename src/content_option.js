1c444178ef03b9bf8cbec5d56679c7eaf64b0ca0
Amal <amaldeepu1@gmail.com>
2024-11-26 15:09:10 -0800

    m

---
+++ src/content_option.js
@@ -0,0 +1,106 @@
+const logotext = "GOK";
+const meta = {
+    title: "Gokul",
+    description: "I'm a Programmer",
+};
+
+const introdata = {
+    title: "Gokul",
+    animated: {
+        first: " I'm a Programmer...",
+        second: "I Love Coding....",
+        third: "Its My Portfolio.....",
+    },
+    description: "I COMPLETED MY GRADUATION FROM  UNIVERSITY INSTITUTE TECHNOLOGY KURAVANKONAM FOR BSC COMPUTER SCIENCE AND I'M APPEARING PG FOR MSC COMPUTER SCIENCE FROM UNIVERSITY OF KERALA KARIAVATTOM.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A Bit About My Self",
+    aboutme: "I COMPLETED MY GRADUATION FROM  UNIVERSITY INSTITUTE TECHNOLOGY KURAVANKONAM FOR BSC COMPUTER SCIENCE AND I'M APPEARING PG FOR MSC COMPUTER SCIENCE FROM UNIVERSITY OF KERALA KARIAVATTOM.",
+};
+const worktimeline = [{
+        jobtitle: "Social Media Marketer",
+        where: "Trivandrum",
+        date: "2024",
+    }
+];
+
+const skills = [{
+        name: "Python",
+        value: 75,
+    },
+    {
+        name: "Djano/css",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 89,
+    },
+    {
+        name: "Jquery",
+        value: 60,
+    },
+];
+
+const services = [{
+        title: "Social Media Marketer",
+        description: "Worked for creating Ads on Social Media Platforms",
+    },
+    {
+        title: "Coding",
+        description: "Skilled in programing.Studied programing languages like React,java,php,SQL,java script,HTML,CSS,Python,C++,C.",
+    },
+    {
+        title: "Image/Video/Audio Editor",
+        description: "Skilled in Editing all kind of video,audio,Banner,Poster etc.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://ubsapp.com/wp-content/uploads/2022/03/attendance_management-copy-1536x891.png",
+        description:"MINOR PROJECT",
+        link: "",
+    },
+    {
+        img: "https://img.freepik.com/free-photo/showing-cart-trolley-shopping-online-sign-graphic_53876-133967.jpg",
+        description: "MAJOR PROJECT",
+        link: "",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "name@domain.com",
+    YOUR_FONE: "7994775442",
+    description: "For Any Query:Contact Me OR Email me.I will Reply ASAP.. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
